360a88d4c55b001b649f268800dba193ff6d2825
Jason <kangja1996@gmail.com>
2021-12-06 00:35:51 -0500

    fix error

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
  import { Component, OnInit } from '@angular/core';
+ import { UiService } from 'src/app/services/ui.service';
+ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +9,15 @@
 })
 export class HeaderComponent implements OnInit {
   title: string = "Task Tracker";
+  showAddTask: boolean = true;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) { }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log("clicked!!!")
+    this.uiService.toggleAddTask()
   }
 } 
